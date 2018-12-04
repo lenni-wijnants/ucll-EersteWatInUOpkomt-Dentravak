6a3dd69f480a5bab2ac469e207419024ff274f42
lenni-wijnants <lenni.wijnants@lennix.be>
2018-12-04 10:55:22 +0100

    gitignore geupdate, build heeft mysql connector dingen, application.yml toegevoegd. eerste test geïmplementeerd

--- src/test/java/be/ucll/ewiuo/controllers/SandwichControllerIntegrationTest.java
+++ src/test/java/be/ucll/ewiuo/controllers/SandwichControllerIntegrationTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
 import static be.ucll.ewiuo.model.SandwichTestBuilder.aSandwich;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
@@ -51,6 +53,16 @@ public class SandwichControllerIntegrationTest extends be.ucll.ewiuo.controllers
 
     @Test
     public void testGetSandwiches_WithSavedSandwiches_ListWithSavedSandwich() throws JSONException {
-        throw new RuntimeException("Implement this test and then the production code");
+
+        Sandwich s1 = new Sandwich("Smoske", "Kaas en Hesp", new BigDecimal(3.50));
+        Sandwich s2 = new Sandwich("Gezond", "Groentjes", new BigDecimal(4.00));
+        Sandwich s3 = new Sandwich("Americain", "Americain en Augurkjes", new BigDecimal(2.20));
+        httpPost("/sandwiches", s1);
+        httpPost("/sandwiches", s2);
+        httpPost("/sandwiches", s3);
+        String sandwiches = httpGet("/sandwiches");
+        String expectedSandwiches = "[{\"id\":\"c561dd56-0641-475a-9092-e33ab3afba4e\",\"name\":\"Smoske\",\"ingredients\":\"Kaas en Hesp\",\"price\":3.50},{\"id\":\"990c64cf-8aa4-4aab-af3c-9294e07c387b\",\"name\":\"Gezond\",\"ingredients\":\"Groentjes\",\"price\":4.00},{\"id\":\"e68ca8b0-8108-473b-8b44-a29d4f4b1a09\",\"name\":\"Americain\",\"ingredients\":\"Americain en Augurkjes\",\"price\":2.20}]";
+
+        assertThatJson(sandwiches).isEqualTo(expectedSandwiches);
     }
 }

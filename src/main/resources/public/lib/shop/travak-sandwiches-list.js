a62b14dc0a2b237afa2c93cc606bf91283aea247
Bjarne <bjarne.huijs@student.ucll.be>
2019-01-04 17:53:34 +0100

    fixed ip misdirects

--- src/main/resources/public/lib/shop/travak-sandwiches-list.js
+++ src/main/resources/public/lib/shop/travak-sandwiches-list.js
@@ -4,7 +4,7 @@ class DenTravakSandwichesList extends DenTravakAbstractElement {
 
     connectedCallback() {
         super.connectedCallback();
-        fetch('/den-travak/api/sandwiches.json')
+        fetch('/den-travak/sandwiches')
             .then(resp => resp.json())
             .then(json => this.updateSandwichesList(json));
     }

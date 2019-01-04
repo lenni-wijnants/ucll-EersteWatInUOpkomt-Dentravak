45b20eb70bf5f75734c2459b0fd38dec77f993a0
Bjarne <bjarne.huijs@student.ucll.be>
2019-01-04 14:17:28 +0100

    db in and output done

--- src/main/java/be/ucll/da/dentravak/repositories/SandwichOrderRepository.java
+++ src/main/java/be/ucll/da/dentravak/repositories/SandwichOrderRepository.java
@@ -1,9 +1,15 @@
 package be.ucll.da.dentravak.repositories;
 
 import be.ucll.da.dentravak.model.SandwichOrder;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface SandwichOrderRepository extends CrudRepository<SandwichOrder, UUID> {
+    @Query("select a from LunchOrder a where SUBSTRING(a.creationDate, 1, 8) = SUBSTRING(:creationDate, 1, 8)")
+    List<SandwichOrder> findAllByDate(@Param("creationDate") LocalDateTime creationDate );
 }
--- src/main/java/be/ucll/da/dentravak/repositories/SandwichRepository.java
+++ src/main/java/be/ucll/da/dentravak/repositories/SandwichRepository.java
@@ -3,8 +3,10 @@ package be.ucll.da.dentravak.repositories;
 import be.ucll.da.dentravak.model.Sandwich;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SandwichRepository extends CrudRepository<Sandwich, UUID> {
+    List<Sandwich> findByName(String name);
 
 }
